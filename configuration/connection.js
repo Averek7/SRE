8e75caad579ae8903941cacbc8f31d1c245da779
Shiva Satapathy <shiva@technoboot.in>
2022-04-18 06:48:46 +0530

    some changes

--- configuration/connection.js
+++ configuration/connection.js
@@ -1,3 +1,3 @@
-prod_url = "mongodb+srv://mongo:mongopassword@technoboot-rmisn.mongodb.net/delhisatta?retryWrites=true&w=majority"
-// prod_url = "mongodb+srv://Skyyrider:android@skyyrider-nawib.mongodb.net/Database?retryWrites=true&w=majority"
+// prod_url = "mongodb+srv://mongo:mongopassword@technoboot-rmisn.mongodb.net/delhisatta?retryWrites=true&w=majority"
+prod_url = "mongodb+srv://mongo:mongopassword@satta.rzil2.mongodb.net/nepaltwo?retryWrites=true&w=majority"
 module.exports = prod_url
