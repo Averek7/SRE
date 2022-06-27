3fb4f5ff62d9375dff9e4fd71b226876ba582be2
Flash <samimdillinger121@gmail.com>
2022-06-27 20:10:30 +0530

    trying to fix error

--- configuration/connection.js
+++ configuration/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // const MONGO_URI = `mongodb://localhost:27017/skyriderexam?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
 
 const connectionMongo = () => {
-  mongoose.connect(MONGO_URI, () => {
+  mongoose.connect(MONGO_URI,{useNewUrlParser : true} ,() => {
     console.log("Database Connected !");
   });
 };
