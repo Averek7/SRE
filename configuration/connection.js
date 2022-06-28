58c06423b6dc0520679dfe0cf287bd2c567fb3c2
Averek <singhavanish2003@gmail.com>
2022-06-28 08:41:39 +0530

    fixes

--- configuration/connection.js
+++ configuration/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
- const MONGO_URI = `mongodb+srv://mongo:mongopassword@technoboot-rmisn.mongodb.net/sreexam?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://mongo:mongopassword@technoboot-rmisn.mongodb.net/sreexam?retryWrites=true&w=majority`;
 // const MONGO_URI = `mongodb://localhost:27017/skyriderexam?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
 
+const connectionMongo = () => {
+  mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((res) => console.log("> Connected..."))
+    .catch((err) =>
+      console.log(`> Error while connecting to mongoDB : ${err.message}`)
+    );
+};
 
-
-module.exports = mongoose.connect(MONGO_URI , { useNewUrlParser : true, useUnifiedTopology : true})
-.then((res)=>console.log('> Connected...'))
-.catch(err=>console.log(`> Error while connecting to mongoDB : ${err.message}`))
-
+module.exports = connectionMongo;
